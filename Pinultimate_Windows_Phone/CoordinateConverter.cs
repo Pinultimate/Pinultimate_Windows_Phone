5c38ba878ee3c494a11298d62ee4f76582a515aa
Firas Abuzaid <fabuzaid21@gmail.com>
2013-03-16 22:30:34 -0700

    Added MapView, shows current location as a blue circle on the map

---
+++ Pinultimate_Windows_Phone/CoordinateConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace Pinultimate_Windows_Phone
+{
+    public static class CoordinateConverter
+    {
+        public static GeoCoordinate ConvertGeocoordinate(Geocoordinate geocoordinate)
+        {
+            return new GeoCoordinate
+                (
+                geocoordinate.Latitude, geocoordinate.Longitude, geocoordinate.Altitude ?? Double.NaN,
+                geocoordinate.Accuracy, geocoordinate.AltitudeAccuracy ?? Double.NaN, geocoordinate.Speed ?? Double.NaN,
+                geocoordinate.Heading ?? Double.NaN
+                );
+
+        }
+    }
+}
--- Pinultimate_Windows_Phone/MainPage.xaml.cs
+++ Pinultimate_Windows_Phone/MainPage.xaml.cs
@@ -1,23 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
+﻿using Microsoft.Phone.Controls;
+using Microsoft.Phone.Maps.Controls;
 using Microsoft.Phone.Shell;
 using Pinultimate_Windows_Phone.Resources;
+using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Devices.Geolocation;
 
 namespace Pinultimate_Windows_Phone
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private async Task<GeoCoordinate> getCurrentLocation()
+        {
+            // Get current location
+            Geolocator locator = new Geolocator();
+            Geoposition position = await locator.GetGeopositionAsync();
+            Geocoordinate coordinate = position.Coordinate;
+            return CoordinateConverter.ConvertGeocoordinate(coordinate);
+        }
+
+        private Ellipse createCircleForCurrentLocation()
+        {
+            Ellipse toReturn = new Ellipse();
+            toReturn.Fill = new SolidColorBrush(Colors.Blue);
+            toReturn.Height = 20;
+            toReturn.Width = 20;
+            toReturn.Opacity = 50;
+            return toReturn;
+        }
+
+        private MapOverlay createMapOverlay(Ellipse circle)
+        {
+            MapOverlay toReturn = new MapOverlay();
+            toReturn.Content = circle;
+            toReturn.PositionOrigin = new Point(0.5, 0.5);
+            return toReturn;
+        }
+
+        private async void showCurrentLocationOnMap()
+        {
+            // Get current location
+            Task<GeoCoordinate> geoCoordinateTask = getCurrentLocation();
+            
+
+            // Create a small circle to mark the current location.
+            Ellipse circle = createCircleForCurrentLocation();
+
+            // Create a MapOverlay to contain the circle.
+            MapOverlay overlay = createMapOverlay(circle);
+
+            // Create a MapLayer to contain the MapOverlay.
+            MapLayer currentLocationLayer = new MapLayer();
+            currentLocationLayer.Add(overlay);
+
+            // Add the MapLayer to the Map
+            GeoCoordinate geoCoordinate = await geoCoordinateTask;
+            PinultimateMap.Center = geoCoordinate;
+            overlay.GeoCoordinate = geoCoordinate;
+            PinultimateMap.Layers.Add(currentLocationLayer);
+        }
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-
+            showCurrentLocationOnMap();
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
         }
