84cb6280cfd6b42a62bffc9cbbb0c8c38315c1ee
Firas Abuzaid <fabuzaid21@gmail.com>
2013-03-16 19:32:19 -0700

    First commit

---
+++ Pinultimate_Windows_Phone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Pinultimate_Windows_Phone.Resources;
+
+namespace Pinultimate_Windows_Phone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
