bbf2b421c5861ceb64870591330843c81ca9d34b
Caiyao Mai <maicaiyao@gmail.com>
2013-04-15 22:27:57 -0700

    get started with VS. More to do in this week.

---
+++ Pinultimate_Windows_Phone/KMeansClustering.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinultimate_Windows_Phone
+{
+    class KMeansClustering
+    {
+        int k;
+
+        public KMeansClustering() { }
+
+        public int K { get; set; }
+
+        public QueryResult<T> data { get; set; }
+
+    }
+}
